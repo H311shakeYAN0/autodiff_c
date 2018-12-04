85768024ce3ac52db224ac2be20c0ad4c66962f8
KouQ7802 <kouq7802@users.noreply.github.com>
2018-12-05 01:17:10 +0900

    initial commit

---
+++ autodiff.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "autodiff.h"
+
+adv advar(double a) {
+  adv x = {a, 0.0};
+  return x;
+}
+
+advl setadvl(unsigned int n, ...) {
+  va_list arglist;
+  unsigned int i;
+  adv **vl = (adv **) malloc(n * sizeof(adv *));
+  vl[0] = (adv *) malloc(n * sizeof(adv *));
+  for (i=1; i<n; ++i) {
+    vl[i] = vl[0] + i;
+  }
+  va_start(arglist, n);
+  for (i=0; i<n; ++i) {
+    vl[i] = va_arg(arglist, adv*);
+  }
+  va_end(arglist);
+  advl advl = {vl, n};
+  return advl;
+}
+
+void adselect(adv *x, advl vl) {
+  unsigned int i;
+  for (i=0; i<vl.len; ++i) {
+    vl.vl[i]->dv = 0.0;
+  }
+  (*x).dv = 1.0;
+}
+
+adv adadd(adv a, adv b) {
+  adv x;
+  x.v = a.v + b.v;
+  x.dv = a.dv + b.dv;
+  return x;
+}
+
+adv adsub(adv a, adv b) {
+  adv x;
+  x.v = a.v - b.v;
+  x.dv = a.dv - b.dv;
+  return x;
+}
+
+adv admul(adv a, adv b) {
+  adv x;
+  x.v = a.v * b.v;
+  x.dv = a.dv*b.v + a.v*b.dv;
+  return x;
+}
+
+adv addiv(adv a, adv b) {
+  adv x;
+  x.v = a.v / b.v;
+  x.dv = (a.dv*b.v - a.v*b.dv)/(b.v * b.v);
+  return x;
+}
+
+adv adpow(adv a, double b) {
+  adv x;
+  x.v = pow(a.v, b);
+  x.dv = b * pow(a.v, b-1.0) * a.dv;
+  return x;
+}
+
+adv adexp(adv a) {
+  adv x;
+  x.v = exp(a.v);
+  x.dv = a.dv * exp(a.v);
+  return x;
+}
+
+adv adlog(adv a) {
+  adv x;
+  x.v = log(a.v);
+  x.dv = a.dv / a.v;
+  return x;
+}
+
+adv adsin(adv a) {
+  adv x;
+  x.v = sin(a.v);
+  x.dv = cos(a.v) * a.dv;
+  return x;
+}
+
+adv adcos(adv a) {
+  adv x;
+  x.v = cos(a.v);
+  x.dv = -sin(a.v) * a.dv;
+  return x;
+}
+
+adv adsum(unsigned int n, ...) {
+  va_list arglist;
+  unsigned int i;
+  adv x = {0.0, 0.0}, temp;
+
+  va_start(arglist, n);
+  for (i=0; i<n; ++i) {
+    temp = va_arg(arglist, adv);
+    x.v += temp.v;
+    x.dv += temp.dv;
+  }
+  va_end(arglist);
+  return x;
+}
---
+++ autodiff.h
@@ -0,0 +1,28 @@
+/*==============================================================================
+  Automatic Differentiation
+==============================================================================*/
+
+#ifndef __AUTODIFF_H__
+#define __AUTODIFF_H__
+typedef struct {
+  double v;
+  double dv;
+} adv;
+typedef struct {
+  adv **vl;
+  unsigned int len;
+} advl;
+adv advar(double a);
+advl setadvl(unsigned int n, ...);
+void adselect(adv *x, advl vl);
+adv adadd(adv a, adv b); // a + b
+adv adsub(adv a, adv b); // a - b
+adv admul(adv a, adv b); // a * b
+adv addiv(adv a, adv b); // a / b
+adv adpow(adv a, double b);
+adv adexp(adv a);
+adv adlog(adv a);
+adv adsin(adv a);
+adv adcos(adv a);
+adv adsum(unsigned int n, ...);
+#endif
