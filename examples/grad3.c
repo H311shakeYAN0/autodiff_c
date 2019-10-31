7cced39055391b0c276451148a3d018a768b3c58
Takayuki Yano <12232024+yano404@users.noreply.github.com>
2019-10-31 18:15:44 +0900

    add examples/grad1~4.c

---
+++ examples/grad3.c
@@ -0,0 +1,119 @@
+#include "../autodiff.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+#define Q1 -1.0
+#define Q1X 0.0
+#define Q1Y 1.0
+
+#define Q2 -2.0
+#define Q2X -0.87
+#define Q2Y -0.5
+
+#define Q3 3.0
+#define Q3X 0.87
+#define Q3Y -0.5
+
+#define N 40
+#define XMIN -4.0
+#define XMAX 4.0
+#define YMIN -4.0
+#define YMAX 4.0
+
+//#define PLT_CONTOUR
+//#define PLT_CONTOUR_N 30
+#define PLT_COLOR // splotでは使えない.
+
+adv phi(adv x, adv y);
+void grad2d(adv (*funcpt)(adv, adv), advl vl, double result[2]);
+void plotscript(char *data);
+
+int main(void) {
+  adv x, y, z;
+  advl vl;
+  double hx, hy;
+  double gradient[2];
+  FILE *fp;
+  fp = fopen("grad3.dat", "w+");
+  vl = setadvl(2, &x, &y);
+  hx = (XMAX - XMIN) / N;
+  hy = (YMAX - YMIN) / N;
+  for (int i = 0; i <= N; i++) {
+    x = advar(XMIN + i * hx);
+    for (int j = 0; j <= N; j++) {
+      y = advar(YMIN + j * hy);
+      z = phi(x, y);
+      grad2d(phi, vl, gradient);
+      fprintf(fp, "%f %f %f %f %f\n", x.v, y.v, z.v, -gradient[0],
+              -gradient[1]);
+    }
+    fputc('\n', fp);
+  }
+  fclose(fp);
+  plotscript("grad3.dat");
+  return 0;
+}
+
+adv phi(adv x, adv y) {
+  return adsum(
+      3,
+      admul(adconst(Q1), adpow(adadd(adpow(adsub(x, adconst(Q1X)), 2.0),
+                                     adpow(adsub(y, adconst(Q1Y)), 2.0)),
+                               -0.5)),
+      admul(adconst(Q2), adpow(adadd(adpow(adsub(x, adconst(Q2X)), 2.0),
+                                     adpow(adsub(y, adconst(Q2Y)), 2.0)),
+                               -0.5)),
+      admul(adconst(Q3), adpow(adadd(adpow(adsub(x, adconst(Q3X)), 2.0),
+                                     adpow(adsub(y, adconst(Q3Y)), 2.0)),
+                               -0.5)));
+}
+
+void grad2d(adv (*funcpt)(adv, adv), advl vl, double result[2]) {
+  adselect(vl.vl[0], vl);
+  result[0] = funcpt(*vl.vl[0], *vl.vl[1]).dv;
+  adselect(vl.vl[1], vl);
+  result[1] = funcpt(*vl.vl[0], *vl.vl[1]).dv;
+}
+
+void plotscript(char *data) {
+  FILE *plt;
+  plt = fopen("grad3.gp", "w+");
+
+  fprintf(plt, "set xrange [%f:%f]\n", XMIN * 1.1, XMAX * 1.1);
+  fprintf(plt, "set yrange [%f:%f]\n", YMIN * 1.1, YMAX * 1.1);
+#ifdef PLT_CONTOUR
+  fputs("set view equal xyz\n", plt);
+  fputs("set contour\n", plt);
+  fputs("unset surface\n", plt);
+  fputs("set view 0, 0\n", plt);
+  fputs("set cntrparam levels auto ", plt);
+#ifdef PLT_CONTOUR_N
+  fprintf(plt, "%d\n", PLT_CONTOUR_N);
+#else
+  fputc('\n', plt);
+#endif
+  fprintf(plt, "splot \"%s\" u 1:2:3 w l\n", data);
+  fprintf(plt,
+          "replot \"%s\" u "
+          "1:2:3:($4/sqrt($4*$4+$5*$5)*0.1):($5/sqrt($4*$4+$5*$5)*0.1):(0) w "
+          "vector ti \"\"\n",
+          data);
+#else
+  fputs("set size ratio -1\n", plt);
+#ifdef PLT_COLOR
+  fprintf(plt,
+          "plot \"%s\" u "
+          "1:2:($4/sqrt($4*$4+$5*$5)*0.1):($5/"
+          "sqrt($4*$4+$5*$5)*0.1):(log10(sqrt($4*$4+$5*$5))) w vector lc "
+          "palette ti \"\"\n",
+          data);
+#else
+  fprintf(plt,
+          "plot \"%s\" u "
+          "1:2:($4/sqrt($4*$4+$5*$5)*0.1):($5/sqrt($4*$4+$5*$5)*0.1) "
+          "w vector ti \"\"\n",
+          data);
+#endif
+#endif
+  fclose(plt);
+}
